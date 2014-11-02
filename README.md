5043d9376cd3fc8b921ee52cf56579451436dc3f
Hugh Nowlan <nosmo@netsoc.tcd.ie>
2014-11-02 11:29:00 +0000

    First commit. I am sorry

---
+++ README.md
@@ -0,0 +1,40 @@
+justDespair
+=======
+
+justDespair is an innovation in guilt-generation
+technologies. justDespair will collect all historical order
+information from just-eat.ie (and hopefully other regions using the
+just-eat platform), total the amount spent over the years and make you
+feel very bad about your life choices.
+
+How to use
+-------
+
+```python justDespair.py my@email.addesss myjusteatpassword```
+
+Passing the ```--json``` command line argument will output a dictionary of the yearly order information as JSON only. No maximums are calculated.
+
+The ```--region``` flag is an untested feature for using justDespair against JustEat sites outside of the default of Ireland.
+
+Library
+-------
+
+justDespair can be used as a simple library as follows:
+
+```
+import justDespair
+just_eat = justDespair.JustEat("glutton@example.com", "some_password")
+order_data = just_eat.getOrderData()
+order_data.keys()
+[2008, 2009, 2010, 2011, 2012, 2013, 2014]
+```
+
+Requirements
+-------
+justDespair requires [requests](http://docs.python-requests.org/en/latest/) and [BeautifulSoup](http://www.crummy.com/software/BeautifulSoup/).
+
+TODO
+-------
+The per-order pages linked from the pages that justDespair currently reads contain even more information about the orders. The number one most embarassing details that I'd like to extract is average time order was placed at (I am thinking late), latest time and earliest time relative to 9AM.
+
+I have no idea how much uniformity there is across different JustEat platforms so I am very interested to see how the --region flag works against sites other than the Irish version.
---
+++ justDespair.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python2
+
+"""
+JustDespair
+ Get historical order information about orders on JustEat. And
+regret. There is no API limit for your own sense of regret.
+
+"""
+
+__author__="nosmo@nosmo.me"
+
+import argparse
+import pprint
+import collections
+import datetime
+
+import BeautifulSoup
+import requests
+
+LOGIN_URL="https://www.just-eat.%s/member/login"
+PREVORDERS_URL="https://www.just-eat.%s/member/prevorders"
+
+class LoginException(Exception):
+    pass
+
+class JustEat(object):
+
+    @staticmethod
+    def generateYearPlaceholder(year):
+        # Generate a placeholder that JustEat expects for the orders page:
+        # 2008 is when the placeholder grouping starts so it is place 00.
+        # for example 2010 will be 04
+        placeholder_t = "ctl00$ContentPlaceHolder1$ctl00$ctl{0:02d}"
+        yearfactor = year - 2008
+        return placeholder_t.format(yearfactor * 2)
+
+    def __init__(self, username, password, region="ie"):
+        self.__username = username
+        self.__password = password
+        self._session = requests.Session()
+        self.region = region
+        self.login()
+
+    def login(self):
+        # Get our session cookies
+        login_attempt = self._session.post(LOGIN_URL % self.region,
+                                           data={"EmailAddress": self.__username,
+                                                 "Password": self.__password,
+                                                 "ReturnUrl": "%2F",
+                                                 "PersistentLoginEnabled": "True",
+                                                 "RememberMe": "false"})
+        if not login_attempt.ok:
+            raise LoginException
+        return True
+
+    def parseOrderPage(self, page_content):
+        # Do some heinous shit with the terrible HTML JustEat return
+        # to get what we need.
+
+        content_reader = BeautifulSoup.BeautifulSoup(page_content)
+        purchases = []
+
+        #this is some vile html: no ids, very little class except for incorrect ones
+        purchase_data = content_reader.findAll("table", {"class":"signUp"})
+        if len(purchase_data) > 1:
+            raise Exception("JustEat's HTML looks worse than usual - something is broken :(")
+        purchase_data = purchase_data[0]
+
+        #TODO use per-order checks with this URL - will be able to get
+        #more interesting details like average time of day (almost
+        #certianly time of morning) of orders
+        #https://www.just-eat.ie/pages/ViewOrder.aspx?ordertable=order&userid=UID&orderid=ORDERID&ratingcode=somestuffidunno
+        for purchase in purchase_data.findAll("tr"):
+            left_td_find = purchase.findAll("td", {"align": "left"})
+            if not left_td_find or len(left_td_find) != 2:
+                #This line isn't a purchase, skip it
+                continue
+
+            #TODO make a datetime
+            purchase_date, purchase_location = [ i.string for i in left_td_find ]
+            purchase_amount = purchase.findAll("div", {"style": "float: right;"})
+            if purchase_amount:
+                purchase_amount = float(purchase_amount[0].string)
+            purchase_id = purchase.findAll("a", {"style": "cursor:pointer;"})
+            if purchase_id:
+                purchase_id = purchase_id[0].string
+            purchase_dict = {
+                "date": purchase_date,
+                "location": purchase_location,
+                "amount": purchase_amount,
+                "id": purchase_id
+            }
+            purchases.append(purchase_dict)
+
+        return purchases
+
+    def getOrderData(self, startyear=2008, endyear=datetime.datetime.now().year):
+        # Request the order history from startyear until endyear and
+        # create a per-year dict.
+        order_dict = collections.defaultdict(dict)
+        for year in range(2008, endyear+1):
+            page_content = self.getOrderPage(year)
+            order_dict[year]["orders"] = self.parseOrderPage(page_content)
+            order_dict[year]["total_cost"] = sum([ i["amount"] for i in order_dict[year]["orders"]])
+            if order_dict[year]["orders"]:
+                order_dict[year]["most_purchased"] = collections.Counter([ i["location"] for i in order_dict[year]["orders"]]).most_common(1)[0][0]
+            else:
+                order_dict[year]["most_purchased"] = None
+
+        return dict(order_dict)
+
+    def getOrderPage(self, year):
+        # Get the HTML content of an order page for a given year
+        year_placeholder = self.generateYearPlaceholder(year)
+        order_attempt = self._session.post(PREVORDERS_URL % self.region,
+                                           data = { "__EVENTTARGET": year_placeholder})
+        if not order_attempt.ok:
+            raise Exception("Couldn't get order page: %s" % order_attempt.content)
+        return order_attempt.content
+
+def main():
+    parser = argparse.ArgumentParser(description='Find out how much more ashamed you should feel about ordering food online')
+    parser.add_argument('email', metavar='email', type=str,
+                        help="Your JustEat account's email address")
+    parser.add_argument("password", metavar="password", type=str,
+                        help="Your JustEat account's password")
+    parser.add_argument("--json", "-j", dest="json", action="store_true",
+                        help="Dump only order output as JSON")
+    parser.add_argument("--region", "-R", dest="region", action="store", default="ie",
+                        help="Region from which to fetch order information (untested)")
+    args = parser.parse_args()
+
+    just_eat = JustEat(args.email, args.password, args.region)
+
+    order_data = just_eat.getOrderData()
+    total_cost = []
+    if args.json:
+        json.dumps(order_data)
+    else:
+        for year, year_details in order_data.iteritems():
+            print("""%d
+            \tMost frequented: %s
+            \tTotal cost: %0.2f""" % (year,
+                                      year_details["most_purchased"],
+                                      year_details["total_cost"] if year_details["total_cost"] \
+                                    else 0.0))
+            total_cost.append(year_details["total_cost"])
+
+        print("\nTotal cost: %0.2f" % sum(total_cost))
+
+if __name__ == "__main__":
+    main()
